0737b4ee6e9f97792f330909660ad1a16483bd81
mytoh <mytoh@gmail.com>
2016-03-23 04:06:15 +0900

    * sis.el: add more functions

--- sis.el
+++ sis.el
@@ -9,6 +9,7 @@
 (require 'pcase)
 
 (cl-defun sis:pair (x y)
+  ;; couple, double, twin, dual
   (cons x y))
 
 (cl-defun sis:fst (p)
@@ -50,6 +51,34 @@
           (pred consp)
           `(,_ . ,(pred (lambda (x) (not (listp x))))))
      t)))
+
+(cl-defun sis:first (f p)
+  (sis:pair (funcall f (sis:fst p))
+          (sis:snd p)))
+
+(cl-defun sis:second(f p)
+  (sis:pair (sis:fst p)
+          (funcall f (sis:snd p))))
+
+(cl-defun sis:bimap(f p)
+  ;; both
+  (sis:pair (funcal f (sis:fst p))
+          (funcal f (sis:snd p))))
+(defalias 'sis:both 'sis:bimap)
+
+(cl-defun sis:fork (f g x)
+  (sis:pair (funcall f x)
+          (funcall g x)))
+(defalias 'sis:&&& 'sis:fork)
+
+(cl-defun sis:product (f g p)
+  (sis:pair (funcall f (sis:fst p))
+          (funcall g (sis:snd p))))
+(defalias 'sis:*** 'sis:product)
+
+
+;; //github.com/ekmett/bifunctiors/old-src/Data
+;; lens, Control-Lens-Tuple
 
 (provide 'sis)
 
